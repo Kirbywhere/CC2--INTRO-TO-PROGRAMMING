f0718293a5d0e2d393a801475bbf4596b118cae8
Kirbywhere <150327197+Kirbywhere@users.noreply.github.com>
2024-10-24 17:26:20 +0800

    Add files via upload

---
+++ First Python Program/LabAct1.py
@@ -0,0 +1,108 @@
+# Pounds to Kilograms
+pounds = 500
+Tokg = 2.205
+Kilo = pounds/Tokg
+
+# Miles to Kilometers
+miles = 50
+Tomi = 1.609
+Kilometer = miles*Tomi
+
+# Farenhiet to Celsius
+Farenhiet = 67
+Tocels = (Farenhiet - 32) * 0.56
+Celsius = Tocels
+
+# Average age of 10 Students
+stu1 = 18
+stu2 = 20
+stu3 = 16
+stu4 = 18
+stu5 = 18
+stu6 = 21
+stu7 = 17
+stu8 = 17
+stu9 = 20
+stu10 = 21
+AveAge = (stu1+stu2+stu3+stu4+stu5+stu6+stu7+stu8+stu9+stu10) /10
+
+# Story: Touhou 6 - Embodiment of Scarlet Devil.
+# Description: The Touhou Project is a popular bullet hell game in japan and even in the west with multiple series (from touhou 1 - touhou 19) and also well known for its banger music tracks.
+Prota1 = "~Reimu Hakurei~"
+Prota2 = "~Marisa Kirisame~"
+Enem1 = "~Rumia~"
+Enem2 = "~Cirno~"
+Enem3 = "~Hong Meiling~"
+Enem4 = "~Patchouli Knowledge~"
+Enem5 = "~Sakuya Izayoi~"
+Enem6 = "~Remilia Scarlet~"
+Location1 = "<Hakurei Shrine>"
+Location2 = "<Forest>"
+Location3 = "<Lake>"
+Location4 = "<Scarlet Devil Mansion>"
+Location5 = "<Library>"
+location7 = "<Gensokyo>"
+wep1 = "<Gohei>"
+wep2 = "<Broomstick>"
+wep3 = "<Mini-Hakkero>"
+wep4 = "<Knives>"
+wep5 = "<spears>"
+Spellcd1 = "<Sun Sign ~ 'Royal Flare'>"
+abil1 = "<Time Manipulation>"
+
+# Prints the original value and the converted value
+print(f"Weight in Pounds(lb):  {pounds}")
+print(f"Weight in Kilograms(kg): {Kilo}")
+print("======================================")
+print(f"Length in Miles(mi): {miles}")
+print(f"Length in Kilometers(km): {Kilometer}")
+print("======================================")
+print(f"Temperature in Farenheit: {Farenhiet}")
+print(f"Temperature in Celsius:  {Celsius}")
+print("======================================")
+
+#Prints the ages of the students and their average age
+print(f"Age of Student 1: {stu1}")
+print(f"Age of Student 2: {stu2}")
+print(f"Age of Student 3: {stu3}")
+print(f"Age of Student 4: {stu4}")
+print(f"Age of Student 5: {stu5}")
+print(f"Age of Student 6: {stu6}")
+print(f"Age of Student 7: {stu7}")
+print(f"Age of Student 8: {stu8}")
+print(f"Age of Student 9: {stu9}")
+print(f"Age of Student 10: {stu10}")
+print(f"The average age of the students is: {AveAge}")
+print("======================================")
+
+# Prints the story 
+print("The Scarlet Mist Incident (Touhou 6 Story)\n")
+
+story = "The story starts at the "  + Location1 +  " where we will find our two protagonists " + Prota1 +  " and "  + Prota2 + ". Reimu, the red-white hakurei shrine maiden is\
+busy sweeping the floors of the hakurei shrine while Marisa, the ordinary magician and Reimu's friend is peacefully sleeping. Their peace was soon interrupted by a\
+strange red mist surrounding the area and the clouds. The mist's source seems to be coming from a mansion near the " + Location3 + ". Reimu and Marisa geared up,\
+Reimu equipping her " + wep1 + " and Marisa with her "+ wep2 + " and " + wep3 + ". They both set off to the mansion and solve the mist incident surrounding " + \
+location7 + " (The place/world this whole story takes place in). Along their way, in the " +Location2 + ", they stumbled upon a cross a yokai named " + Enem1 + " who\
+has the power of darkness manipulation. They introduced themselves then fought. Upon successfully defeating " + Enem1 + ". They reached the " + Location3 + " surrounding\
+the mansion. While at the lake, they are met with a blue ice fairy who manipulates cold named " + Enem2 + ". She cheerfully introduces herself to the two and picks a fight.\
+Due to her lack in power levels, " + Enem2 + " was easily overpowered. They move on to the gates of the mansion and is greeted by the gate keeper " +Enem3+ " who is able\
+to manipulate her life-breath or qi and has a unique physical fighting style. Of course, the two both defeated the gate keeper without breaking a sweat. They now reached\
+the inside of the mansion which is soon to be revealed as the " +Location4+ ". Inside, they entered the "+Location5+" and is met with the mansion's librarian and magician" +\
+Enem4 + ". Unlike the others, she is not worth messing with because she possess powers that she learned from her books and can manipulate the main 5 elements with an\
+addition of the sun and moon's elements. The two protagonists struggled to defeat her and both barely survived after trying to survive Patchouli's powerful spellcard" +\
+Spellcd1 + ". In which she summons a mini sun that burns and disintegrates the surrounding area. Suprisingly, she and her library remained intact but was still defeated.\
+After that, on their way to the mansion's terrace where they wil meet the mistress of the mansion, they were attacked by the mansion's elegant and perfect maid " + Enem5 + ".\
+Who uses " + wep4 + " as her main weapon but can summon infinite amount of knives as well and her main ability is " + abil1+ ". Her weapon and ability is perfect for turning\
+her enemies into a pin cushion. After an excruciating battle, they ended up defeating the maid and proceeded to the terrace. At the terrace, there standing menacingly by the rails\
+the mistress of the " + Location4+ ", the scarlet devil and the one behind the scarlet mist incident, " + Enem6 + ". Remilia who is a vampire, her reason for covering\
+all of "+location7+" in a red mist is so that she can go outside and not get burned to a crisp by the sun. According to Reimu, the mist causes imbalance within " + location7 +\
+"specially for the humans and yokai. Remilia insists that she keeps the mist but Reimu and Marisa refuses, and then they fought. After billions of magical and mystical bullets,\
+along with infinite amounts of " + wep5 + " shot and thrown by Remilia, and also lasers shot at the protagonists. They managed to defeat her and force Remilia to undo the incident\
+After that, everything went back to normal and " +location7+ " was back to normal along with it's beautiful sunny skies. Reimu and Marisa hosted a banquet in celebration to solving\
+the scarlet mist incident. Every yokai, humans, lunarians, fairies, and more came to the shrine even the one who caused the incident, hiding under a parasol " +Enem6 +\
+"and her subbordinates. Reimu soon found out that Remilia has the power to manipulate fate, a very threatening power that prompted Reimu to create the Spellcard rules.\
+The spellcard rules is a complex rule/law for beings living in " +location7+ " weak and strong, to not abuse or use their powerful abilities for their own personal\
+intentions that causes nuisance to others. Reimu is still troubled on why Remilia did not use her ability during battle, but after she digresses, everyone continued\
+their routine and peace was restored once again within "+location7+". This is where the story ends. Coming soon (maybe), Touhou 7's Spring Snow incident."
+print()
+print(story)
